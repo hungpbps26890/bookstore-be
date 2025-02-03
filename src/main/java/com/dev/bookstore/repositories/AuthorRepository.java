5a9270622bb36d058e6da5aedbae0ce2b17b3f56
hungpbps26890 <hungpbps26890@fpt.edu.vn>
2025-02-03 11:19:51 +0700

    Create AuthorRepository and BookRepository

---
+++ src/main/java/com/dev/bookstore/repositories/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.dev.bookstore.repositories;
+
+import com.dev.bookstore.domain.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
